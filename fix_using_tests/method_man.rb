44aa11cf0b8e96ce7b11bae70af782e4fdccbe93
Brad Beggs <brad.beggs@gmail.com>
2020-06-23 22:14:54 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,7 +2,8 @@
 require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{player2}"
+  greeting = "Hello #{player1} & #{player2}"
+  greeting
 end
 
 def play_game(player2)
@@ -12,3 +13,4 @@ def play_game(player2)
 end
 
 start_game("Rocky", "Bullwinkle")
+player1()
