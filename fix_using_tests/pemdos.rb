cd39402a6776cbd65a83b1e9d6841df879363b25
Brad Beggs <brad.beggs@gmail.com>
2020-06-23 22:27:55 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -3,7 +3,9 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    "ssssssssss" + string
+    for counter in 1..9 do
+      "s" + string
+    end  #end the do loop
   else
     string
   end
